00e21f32560475093e3387033be84bfdb8da59fb
zGiusee <giusebelforte@gmail.com>
2023-11-30 14:12:40 +0100

    added the rng generator and combined with bombs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+// CREO LA FUNZIONE CHE GENERI 16 NUMERI CASUALI NON UGUALI TRA LORO
+function rngUnique(bombs_array){
+
+    // Creo la flag
+    let check_num = false;
+    let random_num;
+
+    // Eseguo un ciclo di verifica per i numeri che andranno dentro l'array
+    while(!check_num){
+
+        // Genero i numeri randomici
+        random_num = Math.floor(Math.random() * 100 + 1);
+
+        // Definisco la condizione della verifica, 
+        // in questo caso = Se l'array non ha nessun elemento uguale al numero randomico allora = true
+        if(!bombs_array.includes(random_num)){
+            check_num = true;
+        }
+    }
+
+    return random_num;
+
+}
+
+// CREO LA FUNZIONE CHE GENERI LE BOMBE
+function bombGen(num_of_bombs){
+
+    // Definisco l'array dove andranno inserite le bombe
+    let bombs = [ ];
+
+    // Creo il ciclo che generi le bombe
+    for(i = 0; i<num_of_bombs; i++){
+
+        bombs.push(rngUnique(bombs));
+    }
+
+    return bombs;
+
+}
+
+
 // CREO LA FUNZIONE CHE FACCIA IL SINGOLO QUADRATO DELLA GRIGLIA
 function createSquare(num){
 
@@ -26,6 +67,8 @@ function createSquare(num){
 // DI QUADRATI CHE VOGLIO AVERE NELLA GRIGLIA
 function generateGrid(){
 
+    const num_of_bombs = 16;
+
     griglia.innerHTML = "";
 
     for(let i = 0; i< 100; i++){
@@ -36,6 +79,9 @@ function generateGrid(){
         // Appendo i quadrati alla griglia
         griglia.appendChild(square);
     }    
+
+    const bombs = bombGen(num_of_bombs);
+    console.log(bombs)
 }
 
 // RECUPERO IL CONTENITORE DEI QUADRATI/GRIGLIA
